b24c8ed48db8a9999b8eb4567b7d27ada813ae80
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2021-05-08 14:33:26 -0400

    Reuse the same LBP() for Scene Detections

--- track4-traffic-anomaly-detection/Scene-Change-Stop-Detection/Scene-Stop-Detection/SceneStopDetection.py
+++ track4-traffic-anomaly-detection/Scene-Change-Stop-Detection/Scene-Stop-Detection/SceneStopDetection.py
@@ -6,37 +6,7 @@ import sys
 
 import cv2
 from tqdm import tqdm
-
-
-def LBP(frame):
-    for i in range(len(frame)):
-        for j in range(len(frame[0])):
-            if i==0 or j==0 or i==len(frame)-1 or j==len(frame[0])-1:
-                continue
-            try: 
-                gc=frame[i][j]
-                newvalue=0
-                if frame[i-1][j-1]>=gc:
-                    newvalue+=1
-                if frame[i-1][j]>=gc:
-                    newvalue+=2
-                if frame[i-1][j+1]>=gc:
-                    newvalue+=4
-                if frame[i][j+1]>=gc:
-                    newvalue+=8
-                if frame[i+1][j+1]>=gc:
-                    newvalue+=16
-                if frame[i+1][j]>=gc:
-                    newvalue+=32
-                if frame[i+1][j-1]>=gc:
-                    newvalue+=64
-                if frame[i][j-1]>=gc:
-                    newvalue+=128
-
-                frame[i][j]=newvalue
-            except:
-                print(i, j)
-    return frame
+from ..vid_utils import LBP
 
 
 def getCuts(file_name, cap):
